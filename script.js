5f5f01170902564acd437d18b61cc201c4aa2282
MMartinsR <mariana.martins.rosa.mmr@gmail.com>
2023-08-29 16:25:40 +0100

    Hide parts of the UI when there are no items to be shown, add confirmation to removal

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
-const itemList = document.getElementById('item-list');
+const itemList = document.getElementById('item-list');  // ul
 const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
+
 
 function addItem(e) {
     e.preventDefault(); // Stops the form submiting to the page
@@ -21,7 +23,10 @@ function addItem(e) {
     li.appendChild(document.createTextNode(newItem));    
     li.appendChild(button);
 
+    // Add li to the DOM
     itemList.appendChild(li);
+
+    checkUI();
 
     itemInput.value = '';
 
@@ -46,13 +51,32 @@ function createIcon(classes) {
 
 function removeItem(e) {
     if(e.target.parentElement.classList.contains('remove-item')) {  // button
-        e.target.parentElement.parentElement.remove() // li
+        // Confirm is a function to prompt a confirmation to the user, and its returns true or false depending on the user answer
+        if (confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove() // li  
+            checkUI();          
+        }
     }
 }
 
 function clearItems() {
-    while(itemList.firstChild) {
-        itemList.removeChild(itemList.firstChild);
+
+    if (confirm('Are you sure?')) {
+        while(itemList.firstChild) {
+            itemList.removeChild(itemList.firstChild);
+        }
+        checkUI();
+    }
+}
+
+function checkUI() {
+    const items = itemList.querySelectorAll('li');
+    if (items.length === 0) {
+        clearBtn.style.display = 'none';
+        itemFilter.style.display = 'none';        
+    } else {
+        clearBtn.style.display = 'block';
+        itemFilter.style.display = 'block';
     }
 }
 
@@ -60,3 +84,5 @@ function clearItems() {
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 clearBtn.addEventListener('click', clearItems);
+
+checkUI();
