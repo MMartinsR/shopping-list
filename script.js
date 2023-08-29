8e9b9e1cf2822dec166270f83152ca3b76de2af2
MMartinsR <mariana.martins.rosa.mmr@gmail.com>
2023-08-29 15:57:55 +0100

    Remove single item and clear all items

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
 
 function addItem(e) {
     e.preventDefault(); // Stops the form submiting to the page
@@ -43,5 +44,19 @@ function createIcon(classes) {
     return icon;
 }
 
+function removeItem(e) {
+    if(e.target.parentElement.classList.contains('remove-item')) {  // button
+        e.target.parentElement.parentElement.remove() // li
+    }
+}
+
+function clearItems() {
+    while(itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild);
+    }
+}
+
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
